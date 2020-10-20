bc5f9ec165c45838d0200aec9373cbdaccd6c5f0
MichaelVKrasnov <Michael.V.Krasnov@niuitmo.ru>
2020-10-20 22:14:33 +0300

    Реализована функция encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key = keyword.lower()
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    i1=0
+    for i in plaintext:
+            if alphabet.find(i.lower())!=-1:
+                if i.isupper():
+                    ciphertext+=alphabet[(alphabet.find(i.lower())+alphabet.find(key[i1 % len(key)])) % 26].upper()
+                else:
+                    ciphertext+=alphabet[(alphabet.find(i)+alphabet.find(key[i1 % len(key)])) % 26]
+            else:
+                ciphertext+=i
+            i1+=1
+    return ciphertext
     return ciphertext
 
 
@@ -26,5 +38,16 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    key = keyword.lower()
+    i1=0
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    for i in ciphertext:
+        if alphabet.find(i.lower())!=-1:
+            if i.isupper():
+                plaintext+=alphabet[(alphabet.find(i.lower())-alphabet.find(key[i1 % len(key)])) % 26].upper()
+            else:
+                plaintext+=alphabet[(alphabet.find(i)-alphabet.find(key[i1 % len(key)])) % 26]
+        else:
+            plaintext+=i
+        i1+=1
     return plaintext
