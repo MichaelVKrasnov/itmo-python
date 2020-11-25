47d2f61deaec2c68527339c44a36bba4ce50f095
MichaelVKrasnov <Michael.V.Krasnov@niuitmo.ru>
2020-11-25 07:57:08 +0300

    Done

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,4 +1,5 @@
 from typing import Tuple, List, Set, Optional
+import copy, random
 
 
 def read_sudoku(filename: str) -> List[List[str]]:
@@ -28,7 +29,13 @@ def group(values: List[str], n: int) -> List[List[str]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    a = []
+    for x in range(0, n):
+        b = []
+        for y in range(0, n):
+            b.append(values[x * n + y])
+        a.append(b)
+    return a
 
 
 def get_row(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
@@ -41,7 +48,7 @@ def get_row(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    pass
+    return(grid[pos[0]])
 
 
 def get_col(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
@@ -54,7 +61,8 @@ def get_col(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    pass
+    res = [grid[i][pos[1]] for i in range(0,len(grid))]
+    return(res)
 
 
 def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
@@ -68,7 +76,21 @@ def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    res = []
+    p1 = pos[0]
+    if p1 < 1:
+        p1 = 1
+    if p1 > 7:
+        p1 = 7
+    p2 = pos[1]
+    if p2 < 1:
+        p2 = 1
+    if p2 > 7:
+        p2 = 7
+    for i in range(0,3):
+        for i1 in range(0,3):
+            res.append(grid[p1+i-1][p2+i1-1])
+    return(res)
 
 
 def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
@@ -81,6 +103,15 @@ def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
+    a = 0
+    for i in range(0, len(grid)):
+        for i1 in range(0, len(grid[i])):
+            if grid[i][i1] == '.':
+                a = (i, i1)
+                return(a)
+                break
+        if a != 0:
+            break
     pass
 
 
@@ -95,7 +126,24 @@ def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    a = {'1','2','3','4','5','6','7','8','9'}
+
+    b = get_col(grid, pos)
+    for i in b:
+        if i in a:
+            a.remove(i)
+
+    b = get_row(grid, pos)
+    for i in b:
+        if i in a:
+            a.remove(i)
+
+    b = get_block(grid, (pos[0]//3*3+1,pos[1]//3*3+1))
+    for i in b:
+        if i in a:
+            a.remove(i)
+
+    return(a)
 
 
 def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
@@ -111,13 +159,58 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    pos = find_empty_positions(grid)
+    if pos == None:
+        return(grid)
+    vls = find_possible_values(grid, pos)
+    if vls == {}:
+        return(None)
+    for a in vls:
+        testgrid = copy.deepcopy(grid)
+        testgrid[pos[0]][pos[1]] = a
+        testgrid=solve(testgrid)
+        if testgrid == None:
+            continue
+        return testgrid
 
 
 def check_solution(solution: List[List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
     # TODO: Add doctests with bad puzzles
-    pass
+    for i in range(0,9):
+        a = get_row(solution, (i,0))
+        b = {'1','2','3','4','5','6','7','8','9'}
+        for i1 in a:
+            if i1 in b:
+                b.remove(i1)
+            else:
+                return(False)
+                display(solution)
+                print(i)
+
+    for i in range(0,9):
+        a = get_col(solution, (0,i))
+        b = {'1','2','3','4','5','6','7','8','9'}
+        for i1 in a:
+            if i1 in b:
+                b.remove(i1)
+            else:
+                return(False)
+                display(solution)
+                print(i)
+
+    for i in range(0,9):
+        a = get_block(solution, (i//3*3+1,i%3*3+1))
+        b = {'1','2','3','4','5','6','7','8','9'}
+        for i1 in a:
+            if i1 in b:
+                b.remove(i1)
+            else:
+                return(False)
+                display(solution)
+                print(i)
+
+    return(True)
 
 
 def generate_sudoku(N: int) -> List[List[str]]:
@@ -142,7 +235,38 @@ def generate_sudoku(N: int) -> List[List[str]]:
     >>> check_solution(solution)
     True
     """
-    pass
+    grid = [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
+
+    random.seed()
+
+    """for i in range(0,9):
+
+        for i1 in range(0,2):
+            a = random.randint(0, 2)
+            b = random.randint(0, 2)
+            grid[i1*3+a], grid[i1*3+b] = grid[i1*3+b], grid[i1*3+a]
+
+        for i1 in range(0,2):
+            a = random.randint(0, 2)
+            b = random.randint(0, 2)
+            for i2 in range(0,9):
+                grid[i2][i1*3+a], grid[i2][i1*3+b] = grid[i2][i1*3+b], grid[i2][i1*3+a]
+
+        a = random.randint(0, 2)
+        b = random.randint(0, 2)
+        for i1 in range(0, 2):
+            grid[a*3+i1], grid[b*3+i1] = grid[b*3+i1], grid[a*3+i1]"""
+
+    a = 81-N
+    while a>0:
+        b = random.randint(0, 80)
+        if grid[b//9][b%9] != '.':
+            grid[b//9][b%9] = '.'
+            a -= 1
+
+    """display(grid)
+    print(solve(grid))"""
+    return(grid)
 
 
 if __name__ == '__main__':
